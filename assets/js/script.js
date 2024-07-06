88f0bed538950abe122c51c746bcb1c52c401e5f
Ram Krishna Yadav <rkpy4234@gmail.com>
2024-07-06 20:45:07 +0545

    nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const body = document.querySelector("body"),
        nav = document.querySelector("nav"),
-       modeToggle = document.querySelector(".dark-light");
+       modeToggle = document.querySelector(".dark-light"),
+       searchBox = document.querySelector(".searchBox"),
+       open = document.querySelector(".open"),
+       close = document.querySelector(".close");
+
        
 
 //js code to toggle dark and light mode
@@ -8,3 +12,28 @@ const body = document.querySelector("body"),
         modeToggle.classList.toggle("active");
         body.classList.toggle("dark");
        });
+
+
+
+
+       //toggle for nav bar
+       searchBox.addEventListener("click", ()=>{
+              searchBox.classList.toggle("active");
+              // nav.classList.add("active");
+          });
+      
+      
+      //for side bar
+      
+      open.addEventListener("click",()=>{
+          nav.classList.add("active");
+      })
+      body.addEventListener("click", e =>{
+          let clickedElm = e.target;
+      
+          if(!clickedElm.classList.contains("open") && !clickedElm.classList.contains("menu")){
+              nav.classList.remove("active");
+              searchBox.classList.remove("active");
+          }
+      })
+      
